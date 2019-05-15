e6ce6e48c8fa5bd305770b7b8ed63d14497949c4
alchemistlee <bingyanlyf@gmail.com>
2019-05-15 19:10:57 +0800

    split new

--- toolkit/split_to_train_val_test.py
+++ toolkit/split_to_train_val_test.py
@@ -46,22 +46,28 @@ def produce_train_val_test(in_file,out_train,out_test,out_val,val_rows,test_rows
   test_fh.close()
 
 
-def main(in_file_en,in_file_zh):
+def main(in_file_en,in_file_zh,version=None):
   val_size = int(data_size*rate)
   test_size = int(data_size*rate)
 
   print('val_sample = [ %s ] , test_sample = [ %s ] , all = [ %s ] , rate = [ %s ]',str(val_size),str(test_size),str(data_size),str(rate))
 
-
   val_rows = get_rand_rows(val_size,[])
   test_rows = get_rand_rows(test_size,val_rows)
-
-  train_en=in_file_en+'.train'
-  train_zh=in_file_zh+'.train'
-  val_en=in_file_en+'.val'
-  val_zh=in_file_zh+'.val'
-  test_en=in_file_en+'.test'
-  test_zh=in_file_zh+'.test'
+  if version is None:
+    train_en=in_file_en+'.train'
+    train_zh=in_file_zh+'.train'
+    val_en=in_file_en+'.val'
+    val_zh=in_file_zh+'.val'
+    test_en=in_file_en+'.test'
+    test_zh=in_file_zh+'.test'
+  else :
+    train_en = in_file_en+'-'+version+'-'+'.train'
+    train_zh = in_file_zh + '-'+version+'-'+'.train'
+    val_en = in_file_en + '-'+version+'-'+'.val'
+    val_zh = in_file_zh + '-'+version+'-'+'.val'
+    test_en = in_file_en + '-'+version+'-'+'.test'
+    test_zh = in_file_zh + '-'+version+'-'+'.test'
 
   produce_train_val_test(in_file_en,train_en,test_en,val_en,val_rows,test_rows)
   produce_train_val_test(in_file_zh,train_zh,test_zh,val_zh,val_rows,test_rows)
@@ -71,4 +77,4 @@ def main(in_file_en,in_file_zh):
 if __name__ == '__main__':
   in_file_en = '/root/workspace/data/my_corpus.en.atok'
   in_file_zh = '/root/workspace/data/my_corpus.zh.atok'
-  main(in_file_en,in_file_zh)
+  main(in_file_en,in_file_zh,version='v4')
