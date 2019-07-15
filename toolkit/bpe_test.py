f2b8e84c17aad326cde6158444e09ce7f9936257
yifan.li <yifan.li@tigerobo.com>
2019-07-15 17:09:24 +0800

    up bpe

--- toolkit/bpe_test.py
+++ toolkit/bpe_test.py
@@ -5,6 +5,16 @@
 # @fileName: bpe_test.py
 # @abstract:
 
+import codecs
+from  subword_nmt.apply_bpe import BPE
 
 if __name__ == '__main__':
+    print('hi')
+    c = codecs.open('/root/workspace/translate_data/my_corpus_v6.zh-cut.processed6-bpe-code', encoding='utf-8')
+    m = -1
+    sp = '@@'
+    voc = None
+    a= '保罗 和 哈登'
+    bpe = BPE(c, m, sp, voc, None)
+    print(bpe.process_line(a))
     pass
