dcf73f2b425e00841f9bb5354b929039ecfd0318
alchemistlee <bingyanlyf@gmail.com>
2019-07-16 14:40:43 +0800

    up

--- my_server_enzh.py
+++ my_server_enzh.py
@@ -140,7 +140,7 @@ def tran_en2zh_interface():
     score,pred = _translate(input)
     print(' score = %s , pred = %s ' % (str(score), str(pred)))
     res = {
-      "output": pred.decode('unicode_escape')
+      "output": pred.decode('utf-8')
     }
     return json.dumps(res)
 
